54ff4c3db5aad062895f1c1f7c95f1d039f79a9e
rudgns328 <rudgns328@gmail.com>
2024-08-07 02:09:32 +0900

    [Solve] SET 1_A 18110번 solved.ac by kyunghun

--- src/kyunghun/set1/a_18110/Main.java
+++ src/kyunghun/set1/a_18110/Main.java
@@ -1,2 +1,29 @@
-package kyunghun.set1.a_18110;public class Main {
+package kyunghun.set1.a_18110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        int trim = (int) Math.round(n * 0.15);
+
+        double sum = 0;
+        for (int i = trim; i < n - trim; i++) {
+            sum += arr[i];
+        }
+
+        int avg = (int) Math.round(sum / (n - trim * 2));
+        System.out.print(avg);
+    }
 }
