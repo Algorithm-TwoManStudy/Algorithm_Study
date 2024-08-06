047ab6724c3a996093fb7bf42cf1b4dbea272442
rudgns328 <rudgns328@gmail.com>
2024-08-07 02:07:48 +0900

    [Solve] SET 1_A 1392번 노래 악보 by kyunghun

--- src/kyunghun/set1/a_1392/Main.java
+++ src/kyunghun/set1/a_1392/Main.java
@@ -1,7 +1,36 @@
-package kyunghun.set1.p_1392;
+package kyunghun.set1.a_1392;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int N = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
+            int time = Integer.parseInt(br.readLine());
+
+            for (int j = 0; j < time; j++) {
+                list.add(i);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < Q; i++) {
+            int questionTime = Integer.parseInt(br.readLine());
+
+            sb.append(list.get(questionTime)).append("\n");
+        }
+        System.out.print(sb);
     }
 }
