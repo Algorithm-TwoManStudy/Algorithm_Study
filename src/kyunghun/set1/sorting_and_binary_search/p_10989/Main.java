8fd71805dbee06fbd13cd1453dddff83fbcd5781
rudgns328 <rudgns328@gmail.com>
2024-08-08 00:28:00 +0900

    [Solve] SET1-B 수 정렬하기_3_10989 by kyunghun

--- src/kyunghun/set1/sorting_and_binary_search/p_10989/Main.java
+++ src/kyunghun/set1/sorting_and_binary_search/p_10989/Main.java
@@ -1,2 +1,26 @@
-package kyunghun.set1.sorting_and_binary_search.p_10989;public class Main {
+package kyunghun.set1.sorting_and_binary_search.p_10989;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int[] cnt = new int[10001];
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            cnt[Integer.parseInt(br.readLine())]++;
+        }
+        br.close();
+
+        for (int i = 1; i <= 10000; i++) {
+            while (cnt[i] > 0) {
+                sb.append(i).append('\n');
+                cnt[i]--;
+            }
+        }
+
+        System.out.println(sb);
+    }
 }
