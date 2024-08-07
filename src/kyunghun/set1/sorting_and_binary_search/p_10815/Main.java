6157bf6a2613d827524e6069b0c36d1907dfb090
rudgns328 <rudgns328@gmail.com>
2024-08-08 00:28:22 +0900

    [Solve] SET1-B 숫자 카드_10815 by kyunghun

--- src/kyunghun/set1/sorting_and_binary_search/p_10815/Main.java
+++ src/kyunghun/set1/sorting_and_binary_search/p_10815/Main.java
@@ -1,2 +1,33 @@
-package kyunghun.set1.sorting_and_binary_search.p_10815;public class Main {
+package kyunghun.set1.sorting_and_binary_search.p_10815;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] card = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            card[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(card);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            int ans = Arrays.binarySearch(card, num);
+
+            if (ans >= 0) System.out.print(1 + " ");
+            else System.out.print(0 + " ");
+        }
+    }
 }
