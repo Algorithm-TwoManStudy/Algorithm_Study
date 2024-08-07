73d2603cea1c0d8e97f894f761fd1d9748107775
천영상 <cys07028@naver.com>
2024-08-08 01:07:12 +0900

    [Solve] SET1-B 수 정렬하기 3_10989 by youngsang

--- src/youngsang/set1/sorting_and_binary_search_1/p_10989/Main.java
+++ src/youngsang/set1/sorting_and_binary_search_1/p_10989/Main.java
@@ -1,2 +1,26 @@
-package youngsang.set1.sorting_and_binary_search_1.p_10989;public class Main {
+package youngsang.set1.sorting_and_binary_search_1.p_10989;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();         // 문자열을 만들기 위해서 StringBuilder 객체 생성함
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);       // 오름차순으로 정렬
+
+        for (int i = 0; i < N; i++) {       // 정렬된 정수를 sb에 저장후 줄바꿈
+            sb.append(arr[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
