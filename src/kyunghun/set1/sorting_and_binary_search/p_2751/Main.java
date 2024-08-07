0694783732d3d915a1594c72da4180483d1d0c1a
rudgns328 <rudgns328@gmail.com>
2024-08-08 00:27:37 +0900

    [Solve] SET1-B 수 정렬하기_2_2750 by kyunghun

--- src/kyunghun/set1/sorting_and_binary_search/p_2751/Main.java
+++ src/kyunghun/set1/sorting_and_binary_search/p_2751/Main.java
@@ -1,2 +1,27 @@
-package kyunghun.set1.sorting_and_binary_search.p_2751;public class Main {
+package kyunghun.set1.sorting_and_binary_search.p_2751;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        boolean[] arr = new boolean[2000001];
+        for (int i = 0; i < N; i++) {
+            arr[Integer.parseInt(br.readLine()) + 1000000] = true;
+        }
+        br.close();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]) {
+                sb.append(i - 1000000).append('\n');
+            }
+        }
+        System.out.print(sb);
+    }
 }
