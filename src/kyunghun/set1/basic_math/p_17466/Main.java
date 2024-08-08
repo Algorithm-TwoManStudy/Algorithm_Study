a2702790c72f4868c569d6aa147327f7a71f97d2
rudgns328 <rudgns328@gmail.com>
2024-08-08 23:54:36 +0900

    [Solve] SET1-C N! mod P (1)_17466 by kyunghun

--- src/kyunghun/set1/basic_math/p_17466/Main.java
+++ src/kyunghun/set1/basic_math/p_17466/Main.java
@@ -1,2 +1,20 @@
-package kyunghun.set1.basic_math.p_17466;public class Main {
+package kyunghun.set1.basic_math.p_17466;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        long n = scanner.nextLong();
+        long p = scanner.nextLong();
+
+        long num = 1;
+
+        for (int i = 2; i <= n; i++) {
+            num = (num * i) % p;
+        }
+
+        System.out.println(num);
+    }
 }
