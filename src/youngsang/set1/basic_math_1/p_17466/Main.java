813cfbf55731863df3d39d615ffa199b1859509c
천영상 <cys07028@naver.com>
2024-08-09 00:02:26 +0900

    [Solve] SET1-C N! mod P (1)_17466 by youngsang

--- src/youngsang/set1/basic_math_1/p_17466/Main.java
+++ src/youngsang/set1/basic_math_1/p_17466/Main.java
@@ -1,2 +1,18 @@
-package youngsang.set1.basic_math_1.p_17466;public class Main {
+package youngsang.set1.basic_math_1.p_17466;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long N = sc.nextLong();
+        long P = sc.nextLong();
+
+        long result = 1;
+        for (int i = 1; i <= N; i++) {
+            result = (result * i) % P;
+        }
+        System.out.println(result);
+        sc.close();
+    }
 }
