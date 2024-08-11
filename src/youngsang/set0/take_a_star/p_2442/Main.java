8e161b1777ad42fa6f1cf282ecb2717211cf2a6d
천영상 <cys07028@naver.com>
2024-08-12 02:32:49 +0900

    [Solve] SET0 별 찍기 - 5_2442 by youngsang

--- src/youngsang/set0/take_a_star/p_2442/Main.java
+++ src/youngsang/set0/take_a_star/p_2442/Main.java
@@ -1,2 +1,21 @@
-package youngsang.set0.take_a_star.p_2442;public class Main {
+package youngsang.set0.take_a_star.p_2442;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for(int i = 1; i <= N; i++) {
+            for(int j = i; j <= N - 1; j++)
+                System.out.print(" ");
+            for(int k = 1; k <= i * 2 - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
 }
