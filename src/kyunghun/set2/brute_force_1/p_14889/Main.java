7bfee137690deca0d324867d8ea1c53811ee8eab
rudgns328 <rudgns328@gmail.com>
2024-08-13 01:36:04 +0900

    [Solve] SET2-A 스타트와 링크_14889 by kyunghun

--- src/kyunghun/set2/brute_force_1/p_14889/Main.java
+++ src/kyunghun/set2/brute_force_1/p_14889/Main.java
@@ -1,2 +1,73 @@
-package kyunghun.set2.brute_force_1.p_14889;public class Main {
+package kyunghun.set2.brute_force_1.p_14889;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int[][] map;
+    static boolean[] visit;
+    static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        map = new int[N][N];
+        visit = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        combi(0, 0);
+        System.out.print(min);
+    }
+
+    static void combi(int idx, int count) {
+        if (count == N / 2) {
+            diff();
+            return;
+        }
+
+        for (int i = idx; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                combi(i + 1, count + 1);
+                visit[i] = false;
+            }
+        }
+    }
+
+    static void diff() {
+        int team_start = 0;
+        int team_link = 0;
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (visit[i] && visit[j]) {
+                    team_start += map[i][j];
+                    team_start += map[j][i];
+                } else if (!visit[i] && !visit[j]) {
+                    team_link += map[i][j];
+                    team_link += map[j][i];
+                }
+            }
+        }
+
+        int val = Math.abs(team_start - team_link);
+        if (val == 0) {
+            System.out.print(val);
+            System.exit(0);
+        }
+
+        min = Math.min(val, min);
+    }
 }
